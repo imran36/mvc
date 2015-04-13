3e1df860f1c180a7d5024d1e30d08fb31dc6289c
Imran Chaudhary <imran.chaudhary36@hotmail.com>
2015-04-13 07:05:28 +0500

    yes fresh added

---
+++ MVC_sample_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_sample_Project.Startup))]
+namespace MVC_sample_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
